89c97904a4f5c15b8c0835962290a17c11c2a545
Anlixy <alex19mu9kg26@gmail.com>
2024-04-02 20:49:40 +0300

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QLabel, QRubberBand
 from PyQt5.QtCore import Qt, QRect, QPoint, QSize
 from PIL import ImageGrab
@@ -13,9 +14,17 @@ class ScreenshotApp(QLabel):
         self.rubber_band = QRubberBand(QRubberBand.Rectangle, self)
         self.rubber_band.hide()
 
-        # Загружаем изображение экрана в QLabel
-        screen = QApplication.primaryScreen()
-        pixmap = screen.grabWindow(QApplication.desktop().winId())
+        # Получаем геометрию всего экрана, включая все мониторы
+        desktop = QApplication.desktop()
+        screen_geometry = desktop.screenGeometry()
+
+        # Создаем QPixmap нужного размера
+        pixmap = QPixmap(screen_geometry.size())
+
+        # Делаем скриншот всего экрана
+        pixmap = QApplication.primaryScreen().grabWindow(desktop.winId())
+
+        # Загружаем скриншот в QLabel
         self.setPixmap(pixmap)
 
         # Устанавливаем курсор и события мыши для выделения области экрана
@@ -23,6 +32,9 @@ class ScreenshotApp(QLabel):
         self.setMouseTracking(True)
         self.origin = QPoint()
         self.ex = ex
+
+        # Открываем предпросмотр для выделения области во весь экран
+        self.showFullScreen()
 
     def mousePressEvent(self, event):
         # Сохраняем координаты начала выделения области экрана
@@ -35,20 +47,19 @@ class ScreenshotApp(QLabel):
         self.rubber_band.setGeometry(QRect(self.origin, event.pos()).normalized())
 
     def mouseReleaseEvent(self, event):
-    # Получаем координаты выбранной области экрана в глобальных координатах
+        # Создаем новый QPixmap с выделенной областью
         rect = self.rubber_band.geometry()
-        top_left = self.mapToGlobal(rect.topLeft())
-        bottom_right = self.mapToGlobal(rect.bottomRight())
+        selected_area = self.pixmap().copy(rect)
 
-        # Делаем скриншот выбранной области экрана
-        screenshot = ImageGrab.grab((top_left.x(), top_left.y(), bottom_right.x(), bottom_right.y()))
-
-        # Сохраняем скриншот в файл
-        screenshot.save('temp/screenshot.png')
+        # Сохраняем готовый скриншот в файл
+        selected_area.save('temp/screenshot.png')
         file_path = str(os.getcwd()) + "/temp/screenshot.png"
         # Вызываем метод showDialogWithFile для экземпляра FileDialogExample
         self.ex.showDialogWithFile(file_path)
         self.close()
+
+        # Скрываем выделитель
+        self.rubber_band.hide()
 
 class StartAppS():
     def __init__(self):
